a133735ae31564ceef1730a0159229b7324bd3f9
Betsy Haibel <betsy.haibel@gmail.com>
2018-03-28 17:24:07 -0400

    move the scaling factor from base_depreciation to multiplying it

--- app/jobs/conjured_backstage_pass_quality_update_job.rb
+++ app/jobs/conjured_backstage_pass_quality_update_job.rb
@@ -1,17 +1,17 @@
 class ConjuredBackstagePassQualityUpdateJob < ItemQualityUpdateJob
   def depreciation(item)
     return item.quality if item.sell_in <= 0
-    base_depreciation(item)
+    2 * base_depreciation(item)
   end
 
   def base_depreciation(item)
     case
     when item.sell_in <= 5
-      -3 * 2
+      -3
     when item.sell_in <= 10
-      -2 * 2
+      -2
     else
-      -1 * 2
+      -1
     end
   end
 end
